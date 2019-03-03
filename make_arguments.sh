fd9b30ad4a733e05db2c1b615dbfc85b2bbd62ca
Mikhail Nikolaev (Business Guest) <b-miniko@microsoft.com>
2019-03-03 14:23:50 +0300

    Non linspace excess range for make arguments is implemented

--- make_arguments.sh
+++ make_arguments.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # m(x) excess grid
-origin_k_m=-1.0
-count_k_m=5
-last_k_m=1.0
+km_grid="-1.0 -0.5 0.0 1.0 2.0 4.0"
+count_k_m=6
 
 # m(x) dispersion grid
 origin_d_m=0.05
@@ -11,25 +10,21 @@ count_d_m=15
 last_d_m=0.5
 
 # w(x) excess grid
-origin_k_w=-1.0
-count_k_w=5
-last_k_w=1.0
+kw_grid="-1.0 -0.5 0.0 1.0 2.0 4.0"
+count_k_w=6
 
 # w(x) dispersion grid
 origin_d_w=0.05
 count_d_w=15
 last_d_w=0.5
 
-# space grid
-origin_space=-1
-count_space=100001
-last_space=1
-
 #accurancy
 eps=0.001
 
 m_args="m.txt"
 w_args="w.txt"
+rm -f $m_args $w_args
+temp_file=`mktemp`
 
 args=$1
 if [ -z $args ]
@@ -38,10 +33,16 @@ then
 fi
 
 # WARNING!!! second part was removed due to similar grids for m and w.
-# If you want to cimpute not symmetrical case, please add the second part
+# If you want to compute not symmetrical case, please add the second part
 echo "Creating arguments..."
-./excess $origin_k_m $count_k_m $last_k_m $origin_d_m $count_d_m $last_d_m \
-         100001 $eps $m_args
+for km in $km_grid
+do
+    ./excess $km 1 $km $origin_d_m $count_d_m $last_d_m \
+             100001 $eps $temp_file
+
+    cat $temp_file >> $m_args
+done
+rm -f $temp_file
 
 cp $m_args $w_args
 echo "Collecting them together..."
