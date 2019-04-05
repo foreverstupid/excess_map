ea0f8c6b263cc79d2310a87677b72a1816e3b2ea
Mikhail Nikolaev (Business Guest) <b-miniko@microsoft.com>
2019-04-05 12:49:00 +0300

    Different kernels are added in excess calculator

--- excess_calculator.c
+++ excess_calculator.c
@@ -6,11 +6,26 @@
 
 #define N_COUNT 10000001
 
-inline double kernel(double x, double s0, double s1)
+typedef double (*Kernel)(double, double, double);
+
+double exp_rat_kernel(double x, double s0, double s1)
 {
     double xx = x * x;
     return exp(-0.5 * (s0 * xx + s1 * xx * xx) / (1.0 + xx));
 }
+
+double roughgarden_kernel(double x, double s, double g)
+{
+    return exp(-pow(fabs(x / s), g));    
+}
+
+struct kern_type {
+    char type;
+    Kernel kernel;
+} kern_types[] = {
+    { 'k', &exp_rat_kernel },
+    { 'r', &roughgarden_kernel }
+};
 
 
 
@@ -38,12 +53,12 @@ void calculate(struct vector_func *func, double *k, double *d)
 
 
 
-static double get_begin(double s0, double s1)
+static double get_begin(Kernel kernel, double a, double b)
 {
     double x = 0.0;
     double step = 1e-5;
 
-    while(fabs(kernel(x, s0, s1)) > 1e-12){
+    while(fabs(kernel(x, a, b)) > 1e-12){
         x -= step;
     }
 
@@ -56,25 +71,50 @@ static double get_begin(double s0, double s1)
 
 
 
+Kernel get_kernel(char kern_type)
+{
+    unsigned int i;
+    unsigned int kern_count = sizeof(kern_types) / sizeof(struct kern_type);
+
+    for(i = 0; i < kern_count; i++){
+        if(kern_types[i].type == kern_type){
+            return kern_types[i].kernel;
+        }
+    }
+
+    return NULL;
+}
+
+
+
 int main(int argc, const char **argv)
 {
-    double s0;
-    double s1;
+    double a;
+    double b;
+    char kern;
     double k;
     double d;
     int i;
     double x;
     struct vector_func func;
+    Kernel kernel; 
 
-    sscanf(argv[1], "%lf", &s0);
-    sscanf(argv[2], "%lf", &s1);
+    kern = argv[1][0];
+    kernel = get_kernel(kern);
+    if(kernel == NULL){
+        fprintf(stderr, "### Unknown kernel type!\n");
+        return 1;
+    }
+
+    sscanf(argv[2], "%lf", &a);
+    sscanf(argv[3], "%lf", &b);
 
     func.storage = malloc(sizeof(double) * N_COUNT);
-    func.grid.origin = x = get_begin(s0, s1);
+    func.grid.origin = x = get_begin(kernel, a, b);
     func.grid.count = N_COUNT;
     func.grid.step = 2 * fabs(func.grid.origin) / (N_COUNT - 1);
     for(i = 0; i < N_COUNT; i++){
-        func.storage[i] = kernel(x, s0, s1);
+        func.storage[i] = kernel(x, a, b);
         x += func.grid.step;
     }
 
